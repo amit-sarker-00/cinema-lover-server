60ead471952622b76f1fdb80510f33ad3d34228c
Amit Sarker <amitsarker255@gmail.com>
2023-01-02 01:25:18 +0600

    connect to the monogodb and get some data from database

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qpavz6c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const recentMovies = client.db("onlineMovies").collection("AllMovies");
+
+    app.get("/movies", async (req, res) => {
+      const query = {};
+      const movies = await recentMovies.find(query).toArray();
+      res.json(movies);
+    });
+    app.get("/movies/:status", async (req, res) => {
+      const status = req.params.status;
+      const query = { status: status };
+      const movies = await recentMovies.find(query).toArray();
+      console.log(movies);
+      res.json(movies);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
+
 app.get("/", (req, res) => {
   res.send("Online movie server is running");
 });
